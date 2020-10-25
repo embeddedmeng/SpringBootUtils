4d927f07dfa20cbdbde12f4f3376d09fe0fc7683
mengxy <embeddedmeng@163.com>
2020-10-25 08:19:41 +0800

    +):创建项目

---
+++ src/main/java/com/embeddedmeng/easywork/EasyworkApplication.java
@@ -0,0 +1,13 @@
+package com.embeddedmeng.easywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyworkApplication.class, args);
+    }
+
+}
